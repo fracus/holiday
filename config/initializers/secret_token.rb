922e4cf1d911127f656d7a40563f80de03b53209
fracus <lamontanarofrancisco@hotmail.com>
2012-09-27 11:10:42 -0300

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proy::Application.config.secret_token = 'df1c25d35d0cbe5575c6528e740edca8771bf4268fedfa028d959116a3bb40cb3227ac4b3518be8c45cdf8f4623f35c9b5dc0569b740841ce06a86e9cf7c5c1b'
